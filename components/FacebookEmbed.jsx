311271f12a48f5ffea535c783ccb2defd4345837
Tatsuyi-LittleITCo <165748389+Tatsuyi-LittleITCo@users.noreply.github.com>
2026-03-15 20:27:29 +0800

    Update FacebookEmbed.jsx

--- components/FacebookEmbed.jsx
+++ components/FacebookEmbed.jsx
@@ -1,16 +1,19 @@
 'use client';
-export default function FacebookEmbed({ pageUrl, height = 640 }) {
-  const src = `https://www.facebook.com/plugins/page.php?href=${encodeURIComponent(pageUrl)}&tabs=timeline&width=900&height=${height}&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true`;
+export default function FacebookEmbed({ pageUrl, height = 420 }) {
+  const src = `https://www.facebook.com/plugins/page.php?href=${encodeURIComponent(
+    pageUrl
+  )}&tabs=timeline&width=900&height=${height}&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true`;
+
   return (
-    <div className="card" style={{padding:0, overflow:'hidden'}}>
-      <iframe
-        title="Facebook Page"
-        style={{border:'none', overflow:'hidden', width:'100%', height}}
-        scrolling="no"
-        frameBorder="0"
-        allow="encrypted-media; picture-in-picture; web-share"
-        src={src}
-      />
+    <div className="card" style={{ padding: 0, overflow: 'hidden' }}>
+      <div className="embed-16x9">
+        <iframe
+          title="Facebook Page"
+          src={src}
+          scrolling="no"
+          allow="encrypted-media; picture-in-picture; web-share"
+        />
+      </div>
     </div>
   );
 }
