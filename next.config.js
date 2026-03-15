8b59c316ef4f3691ed34292a3f3c76c496b04c51
Tatsuyi-LittleITCo <165748389+Tatsuyi-LittleITCo@users.noreply.github.com>
2026-03-15 18:51:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',             // produces /out for static hosting
-  images: { unoptimized: true } // allow plain <img> tags
-  // If you ever deploy to a subfolder (e.g., /app), set: basePath: '/app'
+  reactStrictMode: true,
+  output: "export", // <- enables static HTML export
+  trailingSlash: true, // optional, adds trailing slashes to paths
 };
+
 module.exports = nextConfig;
